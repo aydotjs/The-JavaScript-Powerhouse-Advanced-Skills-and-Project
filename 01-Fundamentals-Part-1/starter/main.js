2c9db1c439524e4a925d6713cce7bf3b2dcc7401
aydotjs <ayobami66@gmail.com>
2023-07-26 18:35:38 +0100

    week 11 day 3

---
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -0,0 +1,56 @@
+// alert("Hello world")
+// VALUES AND VARIABLES
+// alert("Ayo")
+// alert(1 + 1)
+// console.log("Ayo");
+// console.log(5 * 8);
+// console.log("Ciroma");
+// let firstName = "Seyi";
+// let lastName = "Owoeye"
+// let myFirstJob = "Programmer"
+// let myCurrentJob = "Teacher"
+// console.log(firstName);
+// CONVENTIONS AND RULES TO NAMING VARIABLES
+// camelCasing
+// variable name should not start with a number
+// let 1stJob = "Programmer"
+// numbers, alphabets, underscore, dollar sign
+// let myFirstName1_$ = "Ayo"
+// no reserved key words
+
+// DATA TYPES
+// 1)primitive
+// a)Number
+// console.log(typeof 5);
+// console.log(typeof 20.54)
+// b)string
+// console.log(typeof "Ciroma");
+
+// console.log("Ciroma");
+// c)Boolean
+// const firstName = "Ayo"
+// console.log(firstname);
+// console.log(typeof true);
+// console.log(false);
+
+// d) undefined
+// let age;
+// console.log(age);
+// console.log(typeof undefined);
+
+// e) Null
+// console.log(typeof null);
+
+// f) symbol
+
+// g) BigInt
+// dynamically typed
+// let age = 20;
+// console.log(typeof age);
+// age = "Ayo"
+// console.log(typeof age);
+
+// LET CONST VAR
+// let age = 20;
+// const firstName = "Ayo"
+// var lastName = "Adekunle"
