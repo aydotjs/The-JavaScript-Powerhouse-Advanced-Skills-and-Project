f496c7ae323e1629eae45f728f45190882f51f74
aydotjs <ayobami66@gmail.com>
2023-08-01 10:56:45 +0100

    week 12 day 1

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -54,3 +54,97 @@
 // let age = 20;
 // const firstName = "Ayo"
 // var lastName = "Adekunle"
+// let firstName = "Ayo"
+// let and const - 2015
+// var firstName =
+// let age = 50;
+// console.log(age);
+// age = 51
+// console.log(age);
+// // let firstName = "Ayo";
+// // firstName = "Seyi"
+
+// let lastName = "Kate";
+// lastName  = "George"
+// let accountBalance;
+// console.log(accountBalance);
+// const
+// const birthYear = 2000;
+// birthYear = 2001
+// const accountBalance;
+
+// var
+// var age = 20;
+// console.log(age);
+// age = 21
+// console.log(age);
+
+// firstName = "Ciroma"
+// console.log(firstName);
+
+// BASIC OPERATORS
+// console.log(2 + 2);
+// Mathematical operators
+// + - * /
+// const ageDumebi = 2023 - 2000;
+// console.log(ageDumebi);
+// console.log(5 + 10);
+// console.log(5 * 10);
+// console.log(5 / 10);
+// exponentiation
+// console.log(5**3);
+// modulus
+// console.log(5 % 2);
+// let x = 5
+// console.log(5 * "Ciroma");
+// console.log("floating" + "berries");
+// comparison operators
+// console.log(5 < 6);//true
+// console.log(5 > 6);
+// console.log(5 <= 6);
+// console.log(5 >= 6);
+// console.log(5 === 6);
+// console.log(5 == 6);
+// = - assignment op
+// == - loose eq op
+// === - strict eq op
+// Assignment operator and Augmented assignment operator
+// let x = 10;
+// console.log(x);
+// x  /= 5;
+// console.log(x);
+
+// Operator precedence
+// console.log(5 * 4 - 10);
+// const weightAyo = 75;
+// const heightAyo = 1.85;
+
+// const BMI = weightAyo / (heightAyo * heightAyo)
+// console.log(BMI);
+
+//TEMPLATE LITERALS
+let balance = 0;
+let newBalance = 10;
+// console.log(
+//   "I woke up with " +
+//     balance +
+//     " and some body gave me money and my new balance is " +
+//     newBalance
+// );
+
+// console.log(
+//   `I woke up with ${balance} and someone gave me some money, now my new balance is ${newBalance}`
+// );
+// console.log(typeof "I am \ngoing home");
+// console.log(typeof `I am
+// going home`);
+
+// taking decisions if/else statments
+const ageCiroma = 20;
+if (ageCiroma >= 18) {
+  console.log(`Come and have your license`);
+} else {
+  console.log(`Wait for ${18 - ageCiroma} years`);
+}
+
+// console.log(1 - "0");
